5d935f3b36f2ad7951c96f55d6a2339e30970640
786964785 <786964785@qq.com>
2017-11-28 21:12:39 +0800

    Index.scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.scss';
+import './styles/Index.scss';
 import App from './routes/Router';
 
 ReactDOM.render(<App />, document.getElementById('root'));
